02ecf5f5541c854c90a8c02d08f387ea57945ad3
Дмитрий Гринь <dzmitry.hryn@gmail.com>
2023-08-03 12:13:22 +0300

    register user

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+import bcrypt from 'bcrypt';
+
+import { registerValidation } from './validations/auth.js';
+import UserModel from './models/User.js';
 
 mongoose
-  .connect('mongodb+srv://admin:1q2w3e@cluster0.sujgw8u.mongodb.net/?retryWrites=true&w=majority')
+  .connect('mongodb+srv://admin:1q2w3e@cluster0.uqyq6us.mongodb.net/blog?retryWrites=true&w=majority')
   .then(() => console.log('DB OK'))
   .catch((err) => console.log('DB error', err));
 
@@ -11,25 +16,49 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());    
+    }
 
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-  const token = jwt.sign(
-    {
+    const doc = new UserModel({
       email: req.body.email,
-      fullName: 'Вася Пупкин',
-    },
-    'secret123',
-  );
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
 
-  res.json({
-    success: true,
-    token,
-  });
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось зарегистрироваться',
+    })
+  }
 });
 
 app.listen(4444, (err) => {
